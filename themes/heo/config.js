dafc88a83e8cab5c8d8afc9a0e56f5999d1dc4df
cunzhangcrypto <cunzhang768@gmail.com>
2026-03-08 21:01:34 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,12 +4,12 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2026-03-7', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到村长的博客', url: '#' },
+    { title: '喜欢我的内容就收藏一下吧！', url: '#' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,14 +18,14 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享资源',
+  HEO_HERO_TITLE_2: '干货知识教程',
+  HEO_HERO_TITLE_3: '#',
+  HEO_HERO_TITLE_4: '战略合作 / Geonix',
+  HEO_HERO_TITLE_5: '全球顶级机房与住宅 IP，网络安全出海首选',
+  HEO_HERO_TITLE_LINK: 'https://geonix.com/?partner_link=hr7qyBUuqy',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  HEO_HERO_COVER_TITLE: '看看村长都有些啥',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -35,17 +35,18 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  HERO_RECOMMEND_COVER: '/geonix-proxy-bg.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '来玩个小游戏',
+    '天王盖地虎',
+    '我猜你会答：宝塔镇河妖',
+    '宫庭玉液酒',
+    '你在想：180一杯',
+    '这啥破玩意儿，没意思',
+    '开个小玩笑，这里啥都没有',
+    '别点了...去看看其它的'
   ],
 
   // 个人资料底部按钮
