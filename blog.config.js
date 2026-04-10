62c320c723251e16e7d1c5ef4caa3b99bc32e17f
cunzhangcrypto <cunzhang768@gmail.com>
2026-04-10 21:51:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,9 +14,9 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '村长', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '分享区块链、AI、网络媒体相关知识！', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://cunzhangblog.com/', // 网站地址
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '村长', // 您的昵称 
+  BIO: process.env.NEXT_PUBLIC_BIO || '分享区块链、AI、网络媒体相关知识，提供技术咨询与视频教程服务。', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://cunzhangblog.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, web3村长, 软件分享, 互联网资源, 区块链, 网站搭建', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
