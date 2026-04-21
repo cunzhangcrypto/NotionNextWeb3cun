3932487907931cf9d8f67077fc7cce2fa69a6e09
cunzhangcrypto <cunzhang768@gmail.com>
2026-04-21 18:47:44 +0800

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,19 @@
 const BLOG = require('./blog.config')
 
-/**
- * 通常没啥用，sitemap交给 /pages/sitemap.xml.js 动态生成
- */
 module.exports = {
   siteUrl: BLOG.LINK,
   changefreq: 'daily',
   priority: 0.7,
   generateRobotsTxt: true,
-  sitemapSize: 7000
-  // ...other options
-  // https://github.com/iamvishnusankar/next-sitemap#configuration-options
+  sitemapSize: 7000,
+  // --- 关键修复代码开始 ---
+  generateIndexSitemap: false, // 除非文章超过7000篇，否则建议设为 false，方便搜索引擎抓取
+  generateRSS: true, // 必须设为 true，才会产出 feed.xml
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `${BLOG.LINK}/sitemap.xml`,
+      `${BLOG.LINK}/feed.xml` // 同时也让 robots.txt 知道 RSS 的存在
+    ]
+  }
+  // --- 关键修复代码结束 ---
 }
